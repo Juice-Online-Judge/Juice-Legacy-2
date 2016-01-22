8528458a3c7b492095118c80ba353dd5c02f8489
DanSnow <dododavid006@gmail.com>
2016-01-22 18:19:54 +0800

    Add db.js

---
+++ database/db.js
@@ -0,0 +1,13 @@
+import Sequelize from 'sequelize';
+import { db } from '../config'
+
+export default new Sequelize(db.database, db.username, db.password, {
+  host: db.host,
+  dialect: db.adapter,
+  pool: {
+    max: 5,
+    min: 0,
+    idle: 10000
+  }
+});
+
