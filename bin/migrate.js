4607b8741454b00bab2c3e7d99feee5cb0d86c27
DanSnow <dododavid006@gmail.com>
2016-01-23 14:09:41 +0800

    Add migrate

---
+++ bin/migrate.js
@@ -0,0 +1,9 @@
+import db from '../models';
+const debug = require('debug')('app:bin:migrate')
+
+for(let key in db) {
+  if(key != 'DB' && db.hasOwnProperty(key)) {
+    debug(`Sync: ${key}`);
+    db[key].sync({ force: true });
+  }
+}
