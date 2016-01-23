65c21df77368efed073496705ee98312454a19d9
DanSnow <dododavid006@gmail.com>
2016-01-24 00:03:28 +0800

    Update model

--- models/user.js
+++ models/user.js
@@ -1,4 +1,7 @@
 import Sequelize from 'sequelize';
+import bcrypt from 'bcrypt';
+import when from 'when';
+import nodefn from 'when/node';
 import db from './db';
 
 export default db.define('user', {
@@ -18,5 +21,25 @@ export default db.define('user', {
     type: Sequelize.STRING(128),
     allowNull: false,
     comment: 'User\'s password hash'
+  }
+}, {
+  paranoid: true,
+  instanceMethods: {
+    setPassword: function (password) {
+      let this_ = this;
+      return when.promise(async (resolve) => {
+        let salt = await nodefn.call(bcrypt.genSalt, 10);
+        let encrypted = await nodefn.call(bcrypt.hash, password, salt);
+        this_.password = encrypted;
+        resolve();
+      });
+    },
+    verifyPassword: function(password) {
+      let this_ = this;
+      return when.promise(async (resolve) => {
+        let res = await nodefn.call(bcrypt.compare, password, this_.password);
+        resolve(res);
+      });
+    }
   }
 });
