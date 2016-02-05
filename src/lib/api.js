76635c6cd3b3a6ee58eeaf71cd2e43feff529280
DanSnow <dododavid006@gmail.com>
2016-02-05 21:13:56 +0800

    Add csrf token; Send cookie default

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,7 +1,12 @@
 import { connectEndpoint } from 'fetch-plus';
+import Cookies from 'js-cookie';
 import plusJson from 'fetch-plus-json';
+import plusCsrf from 'fetch-plus-csrf';
+import cookie from './middleware/cookie';
 
 let api = connectEndpoint('/api')
-  .addMiddleware(plusJson());
+  .addMiddleware(cookie)
+  .addMiddleware(plusJson())
+  .addMiddleware(plusCsrf('X-CSRF-TOKEN', Cookies.get('csrf-token')));
 
 export default api;
