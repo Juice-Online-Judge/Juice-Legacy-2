2e658d358c04ebbe3bef337980759df006932bf7
DanSnow <dododavid006@gmail.com>
2016-02-06 11:59:23 +0800

    Update HomeView

--- src/views/HomeView/HomeView.js
+++ src/views/HomeView/HomeView.js
@@ -1,55 +1,61 @@
 import React, { PropTypes } from 'react';
-import { connect } from 'react-redux';
 import { Link } from 'react-router';
-import { actions as counterActions } from '../../redux/modules/counter';
-import DuckImage from './Duck.jpg';
-import classes from './HomeView.scss';
+import { connect } from 'react-redux';
+
+import AppBar from 'material-ui/lib/app-bar';
+import IconButton from 'material-ui/lib/icon-button';
+import FlatButton from 'material-ui/lib/flat-button';
+import IconMenu from 'material-ui/lib/menus/icon-menu';
+import MoreVertIcon from 'material-ui/lib/svg-icons/navigation/more-vert';
+import MenuItem from 'material-ui/lib/menus/menu-item';
+
+import { actions as authActions } from '../../redux/modules/auth';
 
 // We define mapStateToProps where we'd normally use
 // the @connect decorator so the data requirements are clear upfront, but then
 // export the decorated component after the main class definition so
 // the component can be tested w/ and w/o being connected.
 // See: http://rackt.github.io/redux/docs/recipes/WritingTests.html
-const mapStateToProps = (state) => ({
-  counter: state.counter
-});
 export class HomeView extends React.Component {
   static propTypes = {
-    counter: PropTypes.number.isRequired,
-    doubleAsync: PropTypes.func.isRequired,
-    increment: PropTypes.func.isRequired
+    loginState: PropTypes.object.isRequired,
+    fetchUserInfo: PropTypes.func.isRequired
   };
+
+  componentDidMount() {
+    this.props.fetchUserInfo();
+  }
+
+  get rightMenu() {
+    if (this.props.loginState.get('state')) {
+      return (
+        <IconMenu
+          iconButtonElement={
+            <IconButton><MoreVertIcon /></IconButton>
+          }
+          targetOrigin={{horizontal: 'right', vertical: 'top'}}
+          anchorOrigin={{horizontal: 'right', vertical: 'top'}} >
+          <MenuItem primaryText='Logout' />
+        </IconMenu>
+      );
+    } else {
+      return (
+        <Link to='/signin'>
+          <FlatButton labelStyle={{ color: 'white' }} label='Signin' />
+        </Link>
+      );
+    }
+  }
 
   render() {
     return (
-      <div className='container text-center'>
-        <div className='row'>
-          <div className='col-xs-2 col-xs-offset-5'>
-            <img className={classes.duck}
-              src={DuckImage}
-              alt='This is a duck, because Redux.' />
-          </div>
-        </div>
-        <h1>Welcome to the React Redux Starter Kit</h1>
-        <h2>
-          Sample Counter:
-          {' '}
-          <span className={classes['counter--green']}>{this.props.counter}</span>
-        </h2>
-        <button className='btn btn-default'
-          onClick={() => this.props.increment(1)} >
-          Increment
-        </button>
-        {' '}
-        <button className='btn btn-default'
-          onClick={this.props.doubleAsync} >
-          Double (Async)
-        </button>
-        <hr />
-        <Link to='/404'>Go to 404 Page</Link>
-      </div>
+      <AppBar
+        title='Juice'
+        iconElementRight={ this.rightMenu } />
     );
   }
 }
 
-export default connect(mapStateToProps, counterActions)(HomeView);
+export default connect((state) => {
+  return {loginState: state.auth};
+}, authActions)(HomeView);
