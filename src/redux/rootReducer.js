681d6e387f22d1b1b9e605290196f6d5663b32b1
DanSnow <dododavid006@gmail.com>
2016-02-06 15:49:00 +0800

    Remove counter

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,10 +1,8 @@
 import { combineReducers } from 'redux';
 import { routeReducer as router } from 'redux-simple-router';
-import counter from './modules/counter';
 import auth from './modules/auth';
 
 export default combineReducers({
-  counter,
   auth,
   router
 });
