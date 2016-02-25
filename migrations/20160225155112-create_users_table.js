7642284aa9ac8de430bcccba0ca02dfd7cb97951
BePsvPT <freedom@csie.io>
2016-02-25 23:28:47 +0800

    Create users table migration

---
+++ migrations/20160225155112-create_users_table.js
@@ -0,0 +1,44 @@
+module.exports = {
+  up: function (queryInterface, Sequelize) {
+    return queryInterface.createTable(
+      'users',
+      {
+        id: {
+          type: Sequelize.INTEGER.UNSIGNED,
+          primaryKey: true,
+          autoIncrement: true
+        },
+        username: {
+          type: Sequelize.STRING(16),
+          unique: true,
+          allowNull: false
+        },
+        password: {
+          type: Sequelize.STRING(100),
+          allowNull: false
+        },
+        email: {
+          type: Sequelize.STRING(48)
+        },
+        createdAt: {
+          type: Sequelize.DATE,
+          allowNull: false,
+          field: 'created_at'
+        },
+        updatedAt: {
+          type: Sequelize.DATE,
+          allowNull: false,
+          field: 'updated_at'
+        }
+      },
+      {
+        charset: "utf8mb4",
+        collate: "utf8mb4_unicode_ci"
+      }
+    );
+  },
+
+  down: function (queryInterface, Sequelize) {
+    return queryInterface.dropTable('users');
+  }
+};
